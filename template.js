e5e6d550534e12f541edff0a3df2f6a0d874e4e7
Emre YILDIZ <emreyldz188@gmail.com>
2022-06-01 00:47:18 +0300

    Add files via upload

---
+++ template.js
@@ -0,0 +1,17 @@
+console.log("asd");
+var isim="Emre";
+console.log(isim);
+soyisim="Yldz";
+console.log(isim +" " + soyisim);
+
+_isim="sa";
+console.log(_isim);
+var i;
+window.alert("Ekran Pop-up")
+alert("Pop-up-2");
+var x=10 < 5;
+for( i = 0 ; i<10;i++)
+{
+    console.log("naber "+ isim + " "+ i);
+}
+console.log(x);
