8fa316877ddcc8fd2dcaaea0ebec4a229766b6f7
emreyldz01 <emreyldz188@gmail.com>
2022-06-02 18:51:18 +0300

    done

--- quizApp/script.js
+++ quizApp/script.js
@@ -1,6 +1,6 @@
 const quizData = [
     {
-        question: 'How old are you',
+        question: 'How old are JavaScript',
         a:'10',
         b:'522',
         c:'54',
@@ -26,7 +26,7 @@ const quizData = [
         b:'Hypertext Markup Language',
         c:'Hyper Tank Markup Lecture',
         d: 'Hyper Text Marker Lecture',
-        correct:'a'
+        correct:'b'
     },{
         question:'What year was JavaScript launched ?',
         a:'1990',
@@ -36,37 +36,68 @@ const quizData = [
         correct:'d'
     }
 ]
-const questionEl =document.getElementById("question");
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById('submit');
-
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
-function loadQuiz()
-{
-    const currentQuizData=quizData[currentQuiz];
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
     questionEl.innerText = currentQuizData.question;
-
-    a_text.innerText=currentQuizData.a;
-    b_text.innerText=currentQuizData.b;
-    c_text.innerText=currentQuizData.c;
-    d_text.innerText=currentQuizData.d;
-
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
-submitBtn.addEventListener("click", () =>{
-    currentQuiz++;
-    if(currentQuiz< quizData.length)
-    {
-        loadQuiz();
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
     }
-    else{
-        alert("You finished! Get yourself an Orange ")
-    }
-    loadQuiz();
 });
