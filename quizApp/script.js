ccb5167abbefe26a172159087f772a209582ee0b
emreyldz01 <emreyldz188@gmail.com>
2022-06-02 16:36:08 +0300

    questions

---
+++ quizApp/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old are you',
+        a:'10',
+        b:'522',
+        c:'54',
+        d:'25',
+        correct: 'd'
+    },{
+        question:"What is the most used programing language in 2021",
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct:'d'
+    },{
+        question: 'Who is the President of US?',
+        a:'Florin Pop',
+        b:'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Joe Biden',
+        correct:'d'
+    },{
+        question: 'What does HTML stand for',
+        a:'Hot Topic Master Language',
+        b:'Hypertext Markup Language',
+        c:'Hyper Tank Markup Lecture',
+        d: 'Hyper Text Marker Lecture',
+        correct:'a'
+    },{
+        question:'What year was JavaScript launched ?',
+        a:'1990',
+        b:'2018',
+        c:'1998',
+        d:'none of the above',
+        correct:'d'
+    }
+]
