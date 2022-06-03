44083f5bcabee57f450865a050ae517981671dd5
emreyldz01 <emreyldz188@gmail.com>
2022-06-03 15:15:51 +0300

    son

--- homeworks/template.js
+++ homeworks/template.js
@@ -7,13 +7,11 @@ let passwordInput2 = prompt("re-enter your password","");
 if(passwordInput===passwordInput2){
     go=false;
     alert("şifre dogru");
-    //temp2 =" şifre doğru";
-    //temp.innerHTML = temp2;
-}
-else{
-   // temp2 =" şifre yanlış tekrar dene";
-    //temp.innerHTML = temp2;
-    alert("şifre yanlış");
-}
+    }
+    else{
+  
+        alert("şifre yanlış");
+    }  
+
 
 }
